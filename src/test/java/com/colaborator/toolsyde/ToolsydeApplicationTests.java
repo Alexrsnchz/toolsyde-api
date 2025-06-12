6a3ffbb79bdd9028168397fa40fcbab5f7e23791
Alexrsnchz <77534895+Alexrsnchz@users.noreply.github.com>
2025-06-12 10:42:01 +0200

    build: Add project files

---
+++ src/test/java/com/colaborator/toolsyde/ToolsydeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colaborator.toolsyde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsydeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
