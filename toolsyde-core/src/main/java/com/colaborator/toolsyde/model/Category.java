5013b01be04296d745895c9627f993e16d3dd825
Alexrsnchz <alexrsnchz@gmail.com>
2025-07-15 21:31:10 +0200

    chore: fix slug and color attributes mix

--- toolsyde-core/src/main/java/com/colaborator/toolsyde/model/Category.java
+++ toolsyde-core/src/main/java/com/colaborator/toolsyde/model/Category.java
@@ -1,9 +1,15 @@
 package com.colaborator.toolsyde.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Table(name = "categories")
 public class Category {
@@ -16,64 +22,4 @@ public class Category {
     private String slug;
     private String color;
     private Boolean isNew;
-
-    public Category() {
-    }
-
-    public Category(Long id, String title, String description, String color, String slug, Boolean isNew) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.color = color;
-        this.slug = slug;
-        this.isNew = isNew;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public Boolean getNew() {
-        return isNew;
-    }
-
-    public void setNew(Boolean aNew) {
-        isNew = aNew;
-    }
 }
